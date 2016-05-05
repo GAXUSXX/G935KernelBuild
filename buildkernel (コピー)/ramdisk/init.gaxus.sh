db1a734942b9faecce3f36a154e64fe84fcdefa1
GAXUSXX <mchnet3@gmail.com>
2016-05-05 16:30:06 +0900

    first commit

---
+++ buildkernel (コピー)/ramdisk/init.gaxus.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+mount -o rw,remount /system
+cp /system/extra/init.gaxus.sh /system/init.gaxus.sh
+
---
+++ buildkernel (コピー)/ramdisk/system/extra/init.gaxus.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+mount -o rw,remount /
+mount -o rw,remount /system
+cp /init.gaxus.sh /init.gaxus2.sh
+
